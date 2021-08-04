8c975461e208a93b8d98a7bb63b892e7c33a4093
DESKTOP-42C5TVP\Sipay <bartu.kocakara@sipay.com.tr>
2021-08-05 00:32:07 +0300

    New Models screens added

--- app/Http/Resources/Training.php
+++ app/Http/Resources/Training.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class TrainingEvent extends JsonResource
+class Training extends JsonResource
 {
     /**
      * Transform the resource into an array.
--- database/migrations/2021_08_04_212830_create_trainings_table.php
+++ database/migrations/2021_08_04_212830_create_trainings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTrainingEventsTable extends Migration
+class CreateTrainingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTrainingEventsTable extends Migration
      */
     public function up()
     {
-        Schema::create('training_events', function (Blueprint $table) {
+        Schema::create('trainings', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateTrainingEventsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('training_events');
+        Schema::dropIfExists('trainings');
     }
 }
