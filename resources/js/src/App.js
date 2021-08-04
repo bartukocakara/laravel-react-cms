de0b6a6075cf570e41281003ea1c6ca0919c61f6
DESKTOP-42C5TVP\Sipay <bartu.kocakara@sipay.com.tr>
2021-08-05 00:37:44 +0300

    App component path fixed

--- resources/js/src/App.js
+++ resources/js/src/App.js
@@ -7,9 +7,9 @@ function App() {
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header">App Component</div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">I'm an App component!</div>
                     </div>
                 </div>
             </div>
