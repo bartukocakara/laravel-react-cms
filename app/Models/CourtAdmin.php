08dd12c735e664c3395420767993555adee95b98
DESKTOP-42C5TVP\Sipay <bartu.kocakara@sipay.com.tr>
2021-08-05 00:00:54 +0300

    first commit

---
+++ app/Models/CourtAdmin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CourtAdmin extends Model
+{
+    use HasFactory;
+}
