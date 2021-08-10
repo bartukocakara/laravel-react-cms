f544b9e964c297432781b00985db131dbf72ca0a
DESKTOP-42C5TVP\Sipay <bartu.kocakara@sipay.com.tr>
2021-08-10 18:26:59 +0300

    Models added

---
+++ app/Http/Controllers/MessageTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MessageType;
+use Illuminate\Http\Request;
+
+class MessageTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MessageType  $messageType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MessageType $messageType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MessageType  $messageType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MessageType $messageType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MessageType  $messageType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MessageType $messageType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MessageType  $messageType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MessageType $messageType)
+    {
+        //
+    }
+}
