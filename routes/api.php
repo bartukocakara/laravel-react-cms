17818fe220b3b5d61210c1f9b93b9b6efdf6f606
DESKTOP-42C5TVP\Sipay <bartu.kocakara@sipay.com.tr>
2021-08-12 09:45:12 +0300

    Routes changed

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//***************** BACK APIS ************************
+
 Route::get("/players", "PlayerController@getAll");
 Route::get("/teams", "TeamController@getAll");
 Route::get("/courts", "CourtController@getAll");
@@ -10,6 +12,50 @@ Route::get("/matches", "MatchController@getAll");
 Route::get("/trainings", "TrainingController@getAll");
 Route::get("/trainers", "TrainerController@getAll");
 Route::get("/tournaments", "TournamentController@getAll");
+
+
+
+// ***************** FRONT APIS ************************
+
+                    // PLAYER
+Route::get("/{id}/lasActivities", "PlayerController@getPlayerLastActivities");
+Route::get("/{id}/favouritePlayers", "PlayerController@getFavouritePlayers");
+Route::get("/{id}/favouriteTeams", "PlayerController@getFavouriteTeams");
+Route::get("/{id}/favouriteTrainers", "PlayerController@getFavouriteTrainers");
+Route::get("/{id}/favouriteCourts", "PlayerController@getFavouriteCourts");
+Route::get("/{id}/matches", "PlayerController@getPlayersMatches");
+Route::get("/{id}/trainings", "PlayerController@getPlayersTrainings");
+
+
+
+                    // TEAM
+
+
+
+                    //COURT
+
+
+
+                    //MATCH
+
+
+
+                    //TRAINING
+
+
+
+                    //TRAINER
+
+
+                    //TOURNAMENT
+
+
+
+                    //COURT HOST
+
+
+
+                    //ALL STAR
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
