a0b2f40bf8c968a3048dcb95c423afd65f73f5eb
DESKTOP-42C5TVP\Sipay <bartu.kocakara@sipay.com.tr>
2021-08-12 13:02:53 +0300

    api routes categorized v1.0

--- routes/api.php
+++ routes/api.php
@@ -4,28 +4,78 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //***************** BACK APIS ************************
+    // -----route prefix urı (/back/)----
 
+                    // PLAYER
 Route::get("/players", "PlayerController@getAll");
+Route::get("/players/create", "PlayerController@createPlayerPage");
+Route::post("/players/create", "PlayerController@createPlayerPost");
+
+                    // TEAM
 Route::get("/teams", "TeamController@getAll");
+Route::get("/teams/create", "TeamController@createTeamPage");
+Route::post("/teams/create", "TeamController@createTeamPost");
+
+                    //COURT
 Route::get("/courts", "CourtController@getAll");
+Route::get("/courts/create", "CourtController@createCourtPage");
+Route::post("/courts/create", "CourtController@createCourtPost");
+
+                    //MATCH
 Route::get("/matches", "MatchController@getAll");
+Route::get("/matches/create", "MatchController@createMatchPage");
+Route::post("/matches/create", "MatchController@createMatchPost");
+
+                    //TRAINING
 Route::get("/trainings", "TrainingController@getAll");
+Route::get("/trainings/create", "TrainingController@createTrainingPage");
+Route::post("/trainings/create", "TrainingController@createTrainingPost");
+
+                    //TRAINER
 Route::get("/trainers", "TrainerController@getAll");
+Route::get("/trainers/create", "TrainerController@createTrainerPage");
+Route::post("/trainers/create", "TrainerController@createTrainerPost");
+
+
+                    //TOURNAMENT
 Route::get("/tournaments", "TournamentController@getAll");
+Route::get("/tournaments/create", "TournamentController@createTournamentPost");
+Route::post("/tournaments/create", "TournamentController@createTournamentPage");
+
+
+                    //COURT HOST
+Route::get("/courtHosts", "CourtHostController@getAll");
+Route::get("/courtHosts/create", "CourtHostController@createCourtHostPage");
+Route::post("/courtHosts/create", "CourtHostController@createCourtHostPost");
+
+
+                    //ALL STAR
+Route::get("/allStars", "AllStarController@getAll");
+Route::get("/allStars/create", "AllStarController@createAllStarPage");
+Route::psot("/allStars/create", "AllStarController@createAllStarPost");
+
+
+                    //REFEREE
+Route::get("/referees", "RefereeController@getAll");
+Route::get("/referees/create", "AllStarController@createRefereePage");
+Route::post("/referees/create", "AllStarController@createRefereePost");
 
 
 
 // ***************** FRONT APIS ************************
 
                     // PLAYER
-Route::get("/{id}/lasActivities", "PlayerController@getPlayerLastActivities");
-Route::get("/{id}/favouritePlayers", "PlayerController@getFavouritePlayers");
-Route::get("/{id}/favouriteTeams", "PlayerController@getFavouriteTeams");
-Route::get("/{id}/favouriteTrainers", "PlayerController@getFavouriteTrainers");
-Route::get("/{id}/favouriteCourts", "PlayerController@getFavouriteCourts");
-Route::get("/{id}/matches", "PlayerController@getPlayersMatches");
-Route::get("/{id}/trainings", "PlayerController@getPlayersTrainings");
+Route::namespace('Api')->group(function () {
 
+    Route::get("/{id}/lasActivities", "PlayerController@getPlayerLastActivities");
+    Route::get("/{id}/favouritePlayers", "PlayerController@getFavouritePlayers");
+    Route::get("/{id}/favouriteTeams", "PlayerController@getFavouriteTeams");
+    Route::get("/{id}/favouriteTrainers", "PlayerController@getFavouriteTrainers");
+    Route::get("/{id}/favouriteCourts", "PlayerController@getFavouriteCourts");
+    Route::get("/{id}/matches", "PlayerController@getPlayersMatches");
+    Route::get("/{id}/trainings", "PlayerController@getPlayersTrainings");
+
+});
 
 
                     // TEAM
