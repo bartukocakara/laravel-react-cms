dcdb3c351cc6fb0f0750befce7e7c2649b62e3cb
bartukocakara <kocakarabartu@gmail.com>
2021-08-28 01:38:42 +0300

    m s f r added

---
+++ app/Http/Controllers/TrainingTypeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TrainingType;
+use Illuminate\Http\Request;
+
+class TrainingTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TrainingType  $trainingType
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TrainingType $trainingType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TrainingType  $trainingType
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TrainingType $trainingType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TrainingType  $trainingType
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TrainingType $trainingType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TrainingType  $trainingType
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TrainingType $trainingType)
+    {
+        //
+    }
+}
---
+++ database/factories/TraininigItemFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TraininigItem;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TraininigItemFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TraininigItem::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
